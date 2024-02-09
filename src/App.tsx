2d160005352b609bf57e9d237c50d248c9658c5b
Lisk <liskqwq@gmail.com>
2024-02-09 13:09:51 +0800

    feat: edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import hero from './assets/hero.png'
 import './App.css'
-function clickHandler(e){
-  e.target.style.filter="none"
-}
-//document.querySelector(".logo").addEventListener("click", clickHandler)
+
 function App() {
   return (
     <>
-    <div onclick={clickHandler} class="hero">
+    <div class="hero">
     <img src={hero} className="logo" />
     </div>
     <footer>Copyright © 2024 Lisk All rights reserved.</footer>
