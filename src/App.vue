09b906faa68ff96a2260e2ba8c31577689e76744
Lisk <liskqwq@gmail.com>
2024-02-11 14:39:27 +0800

    reconstruction with vue

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+//import HelloWorld from './components/HelloWorld.vue'
+import {ref, watch} from 'vue'
+const isGray=ref(false)
+watch(isGray, (now)=>{
+  let html=document.querySelector(":root") as HTMLElement
+  // @ts-ignore
+  html.style["background-color"]=now?"#000":"#fff"
+})
+</script>
+
+<template>
+	<div class="hero" :style="`background-color:${isGray?'#161b21':'#f0f0f0'};`">
+	  <img @click="isGray=!isGray" :style="`filter:drop-shadow(0 0 4em #688978)${isGray?' grayscale(1)':';'}`" :src="`/hero${isGray?'_':''}.png`" class="logo" />
+  </div>
+  <footer>Copyright © 2024 Lisk All rights reserved.</footer>
+</template>
+
+<style scoped>
+.hero {
+  border-radius:30px;
+}
+footer {
+  color:#555;
+}
+.logo {
+  height: 100%;
+  width:100%;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  /*filter: drop-shadow(0 0 4em #688978);*/
+}
+</style>
